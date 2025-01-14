91c0a19cb771ba528d9f0966e0ef7caab46f75f9
tain-jorge <jorge.miranda@tain.com>
2025-01-14 10:58:42 +0000

    Roadmap page

--- src/pages/Roadmap.tsx
+++ src/pages/Roadmap.tsx
@@ -51,10 +51,11 @@ const typographyStyles = {
     mb: 1,
   },
   roadmapDescription: {
-    fontWeight: 400,
+    fontWeight: 300,
     fontSize: '24px',
     lineHeight: '36px',
     fontFamily: 'Raleway, serif',
+    color: '#FFFFFF',
     '& span': { fontWeight: 700 },
   },
   callToAction: {
@@ -86,14 +87,18 @@ const containerStyles = {
     mx: 'auto',
   },
   roadmapItemWrapper: {
-    position: 'relative',
-    height: '100%',
     backgroundColor: 'transparent',
-    // borderRadius: 25 * 2 + 'px',
-    // border: `6px solid ${secondaryColor}`,
     transition: 'all 0.3s ease',
     zIndex: 2,
     textAlign: 'center',
+    position: 'absolute',
+    p: 4,
+    py: 5,
+    height: 'max-content',
+    boxSizing: 'border-box',
+    minHeight: '400px',
+    borderLeft: `6px solid ${secondaryColor}`,
+    borderRight: `6px solid ${secondaryColor}`, 
   },
   connectingLine: {
     position: 'absolute',
@@ -122,47 +127,39 @@ const containerStyles = {
     fontSize: '24px',
     zIndex: 4,
   },
-  phasesWrapper: {
-    border: '1px solid #7C1DF7',
-    borderRadius: '40px',
-    padding: '16px',
-    marginTop: '60px',
-    width: 'calc(100% - 32px)',
-  },
-  phasesInnerBg: {
-    display: 'flex',
-    background: 'linear-gradient(172.92deg, rgba(0, 0, 0, 0.1) 5.53%, rgba(90, 53, 151, 0.2) 89.21%)',
-    width: 'calc(100% - 120px)',
-    height: 'calc(100% - 120px)',
-    borderRadius: '30px',
-    padding: '60px',
+  ctaWrapper: {
+    padding: '0 1rem',
+    color: '#FFFFFF',
+    fontFamily: 'Roboto',
+    overflowX: 'hidden',
   },
 };
 
 const Roadmap: React.FC = () => {
   return (
-    <Box sx={containerStyles.mainWrapper}>
-      <AnimatedSection>
-        <Box display="flex" flexDirection="column" gap={2}>
-          <Box display="flex" justifyContent="space-between">
-            <Box sx={{ width: '48%' }}>
-              <Typography variant="h3" sx={typographyStyles.header}>
-                Roadmap<br/><span>2025</span>
-              </Typography>
-            </Box>
-            <Box sx={{ width: '48%', display: 'flex', alignItems: 'flex-end' }}>
-              <Typography variant="body1" sx={typographyStyles.subHeader}>
-                Gamify your strategies with new exciting features this year
-              </Typography>
+    <>
+      <Box sx={containerStyles.mainWrapper}>
+        <AnimatedSection>
+          <Box display="flex" flexDirection="column" gap={2}>
+            <Box display="flex" justifyContent="space-between">
+              <Box sx={{ width: '48%' }}>
+                <Typography variant="h3" sx={typographyStyles.header}>
+                  Roadmap<br/><span>2025</span>
+                </Typography>
+              </Box>
+              <Box sx={{ width: '48%', display: 'flex', alignItems: 'flex-end' }}>
+                <Typography variant="body1" sx={typographyStyles.subHeader}>
+                  Gamify your strategies with new exciting features this year
+                </Typography>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </AnimatedSection>
-      
+        </AnimatedSection>
+      </Box>
       <AnimatedSection>
         <Box sx={{ position: 'relative', overflow: 'hidden' }} py={8}>
           {/* Now dot and label */}
-          <Box sx={{ position: 'absolute', left: '5px', top: '490px' }}>
+          <Box sx={{ position: 'absolute', left: '7.5px', top: '490px' }}>
             <Box sx={containerStyles.nowDot} />
             <Typography sx={containerStyles.nowLabel}>Now</Typography>
           </Box>
@@ -206,26 +203,32 @@ const Roadmap: React.FC = () => {
                 <CardContent
                   sx={{
                     ...containerStyles.roadmapItemWrapper,
-                    position: 'absolute',
-                    width: index === 0 ? 'calc(100% - 25px)' : '100%',
                     top: item.top,
-                    p: 2,
-                    py: 6,
-                    height: 'max-content',
-                    // border: '1px solid #7C1DF7',
-                    boxSizing: 'border-box',
-                    minHeight: '400px',
-                    left: index === 0 ? '25px' : 0,
-                    borderLeft: `6px solid ${secondaryColor}`,
-                    borderRight: `6px solid ${secondaryColor}`, 
+                    width: index % 2 === 0 ? '100%' : 'calc(100% + 10px)%',
+                    left: index === 0 ? '25px' : index >= 2 ? '0px' : '25px',
                     borderTop: index % 2 === 0 ? `6px solid ${secondaryColor}` : 'none',
                     borderBottom: index % 2 === 0 ? 'none' : `6px solid ${secondaryColor}`,
                     borderTopLeftRadius: index % 2 === 0 ? '50%' : '0px',
                     borderTopRightRadius: index % 2 === 0 ? '50%' : '0px',
                     borderBottomLeftRadius: index % 2 === 0 ? '0px' : '50%',
                     borderBottomRightRadius: index % 2 === 0 ? '0px' : '50%',
-                    '&:before': {},
-                    '&:after': {}
+                    margin: index % 2 === 0 ? '0px 0px 0px 0px' : '0px -6px 0px -6px',
+                    '&:before': {
+                      content: '""',
+                      position: 'absolute',
+                      left: '-27.5px',
+                      width: 'calc(100% + 55px)',
+                      height: '100%',
+                      top: index % 2 === 0 ? '-50px' : '50px',
+                      
+                      borderTop: index % 2 === 0 ? `6px dashed ${secondaryColor.replace(/,/g, ', ')}` : 'none',
+                      borderBottom: index % 2 === 0 ? 'none' : `6px dashed ${secondaryColor.replace(/,/g, ', ')}`,
+                      borderTopLeftRadius: index % 2 === 0 ? '50%' : '0px',
+                      borderTopRightRadius: index % 2 === 0 ? '50%' : '0px',
+                      borderBottomLeftRadius: index % 2 === 0 ? '0px' : '50%',
+                      borderBottomRightRadius: index % 2 === 0 ? '0px' : '50%',
+                      margin: index % 2 === 0 ? '0px 0px 0px 0px' : '0px -6px 0px -6px',
+                    },
                   }}
                 >
                   <Box sx={containerStyles.iconWrapper}>
@@ -248,26 +251,26 @@ const Roadmap: React.FC = () => {
           </Grid2>
         </Box> 
       </AnimatedSection>
-
-      <AnimatedSection>
-        <Box
-          component={motion.div}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 1.2 }}
-          textAlign="center"
-          mt={8}
-          p={4}
-        >
-          <Typography
-            sx={typographyStyles.callToAction}
+      <Box sx={containerStyles.ctaWrapper}>
+        <AnimatedSection>
+          <Box
+            component={motion.div}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.2 }}
+            textAlign="center"
+            p={4}
           >
-            Book a call{" "}
-            <span>with us today and let's achieve your goals together!</span>
-          </Typography>
-        </Box>
-      </AnimatedSection>
-    </Box>
+            <Typography
+              sx={typographyStyles.callToAction}
+            >
+              Book a call{" "}
+              <span>with us today and let's achieve your goals together!</span>
+            </Typography>
+          </Box>
+        </AnimatedSection>
+      </Box>
+    </>
   );
 };
 
